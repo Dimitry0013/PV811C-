a8205442ffb261c69028728b0cc91ea041a8acf6
Clayman <Clayman@DESKTOP-DM8E77U>
2020-04-25 12:50:43 +0300

    Replacement DONE.

--- WinAPI/TextFields/main.cpp
+++ WinAPI/TextFields/main.cpp
@@ -6,6 +6,8 @@ HWND hEdit2;
 
 CHAR str1[] = "Hello guys ;-)";
 CHAR str2[] = { 0 };
+CONST UINT MSG_SIZE = 256;
+CONST CHAR sz_invitation[MSG_SIZE] = "Type your name";
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -28,7 +30,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		hEdit2 = GetDlgItem(hwnd, IDC_EDIT2);
 
 		//SendMessage(hEdit1, WM_SETTEXT, 0, (LPARAM)str1);
-		SetWindowText(hEdit1, str1);
+		SetWindowText(hEdit1, sz_invitation);
 
 		SetFocus(GetDlgItem(hwnd, IDC_COPY));
 	}
@@ -36,6 +38,23 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_EDIT1:
+		{
+			CHAR buffer[256] = {};
+			GetDlgItemText(hwnd, IDC_EDIT1, buffer, 256);
+			if (HIWORD(wParam) == EN_SETFOCUS)
+			{
+				if (strcmp(buffer, sz_invitation) == 0)SetDlgItemText(hwnd, IDC_EDIT1, "");
+			}
+			if (HIWORD(wParam) == EN_KILLFOCUS)
+			{
+				if (strcmp(buffer, "") == 0)
+				{
+					SetDlgItemText(hwnd, IDC_EDIT1, sz_invitation);
+				}
+			}
+		}
+		break;
 		case IDC_COPY:
 			/*SendMessage(hEdit1, WM_GETTEXT, 255, (LPARAM)str2);
 			SendMessage(hEdit2, WM_SETTEXT, 0, (LPARAM)str2);*/
@@ -43,8 +62,15 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			SetWindowText(hEdit2, str2);
 			break;
 		case IDOK:
-			MessageBox(hwnd, "Hello", "Hi", MB_OK);
-			break;
+		{
+			CHAR msg[MSG_SIZE] = "Hello ";
+			CHAR buffer[MSG_SIZE]{};
+			GetDlgItemText(hwnd, IDC_EDIT1, buffer, MSG_SIZE);
+			strcat_s(msg, MSG_SIZE, buffer);
+			strcat_s(msg, MSG_SIZE, "!");
+			MessageBox(hwnd, msg, "Hi", MB_OK);
+		}
+		break;
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
 		}
@@ -53,4 +79,4 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		EndDialog(hwnd, 0);
 	}
 	return FALSE;
-}	
+}
